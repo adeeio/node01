89b8ae71b5d199a1f176d35c65f29e020d6a4a72
Adrian Ostapkiewicz <ostapkiewicz.adrian@gmail.com>
2019-07-31 18:18:49 +0100

    merged with task 17.5

--- modules/convertTime.js
+++ modules/convertTime.js
@@ -3,8 +3,8 @@ var os = require('os');
 function getTime(SECONDS) {
     var date = new Date(null);
     date.setSeconds(SECONDS);
-    var resoult = date.toISOString().substr(11, 8);
-    console.log(resoult[0] + resoult[1] +" godz. " + resoult[3] + resoult[4] + " min. " + resoult[6] + resoult[7] + " sek.");   
+    var result = date.toISOString().substr(11, 8);
+    console.log(result[0] + result[1] +" godz. " + result[3] + result[4] + " min. " + result[6] + result[7] + " sek.");   
 }
 
 exports.print = getTime;
